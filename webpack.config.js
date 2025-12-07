710d956d032b876d7dfa8a569ad92cde8f76b978
David-Shayne <63462420+David-Shayne@users.noreply.github.com>
2025-12-07 13:38:58 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+// webpack.config.js
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+
+	plugins: [
+		new htmlWebpackPlugin({
+			template: "./src/index.html",
+		}),
+	],
+
+	devtool: "eval-source-map",
+	devServer: {
+		watchFiles: ["./src/template.html"],
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+};
